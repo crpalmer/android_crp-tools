c3b6640f30b2c0cdf2580283edc1f27108c68a21
Christopher R. Palmer <crpalmer@gmail.com>
2016-05-02 05:50:06 -0400

    cpu-stats: Fix handling of cpu number
    Change-Id: I4b4addca84e65c2cba073481ad69fbc8c427f3dd

--- cpu-stats.c
+++ cpu-stats.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
         FILE *f;
 
         if ((f = fopen("/proc/stat", "r")) != NULL) {
-            int cpu = 0;
+            int cpu_id = 0;
 
             printf("%ld", (unsigned long) time(NULL));
             while(fgets(buf, sizeof(buf), f) != NULL) {
@@ -27,37 +27,39 @@ int main(int argc, char **argv)
                         FILE *f2;
                         unsigned long long user, nice, system, idle, iowait, irq, softirq;
                         unsigned long long active;
+                        int cpu_nr = cpu_id - 1;
 
-                        sprintf(buf, "/sys/devices/system/cpu/cpu%d/online", cpu);
+                        sprintf(buf, "/sys/devices/system/cpu/cpu%d/online", cpu_nr);
                         if ((f2 = fopen(buf, "r")) != NULL) {
                                 int online = 1;
                                 fscanf(f2, "%d", &online);
                                 fclose(f2);
                                 if (! online) {
-                                        continue;
+                                    cpu_id++;
+                                    continue;
                                 }
                         }
                         sscanf(buf, "%*s %lld %lld %lld %lld %lld %lld %lld", &user, &nice, &system, &idle, &iowait, &irq, &softirq);
                         active = user + nice + system;
                         irq += softirq;
-                        if (cpu == 0) {
-                                unsigned long long delta_active = (active + iowait + irq) - (cpu_stats[cpu].active + cpu_stats[cpu].iowait + cpu_stats[cpu].irq);
-                                unsigned long long delta_idle = idle - cpu_stats[cpu].idle;
+                        if (cpu_id == 0) {
+                                unsigned long long delta_active = (active + iowait + irq) - (cpu_stats[cpu_id].active + cpu_stats[cpu_id].iowait + cpu_stats[cpu_id].irq);
+                                unsigned long long delta_idle = idle - cpu_stats[cpu_id].idle;
                                 printf(" %5.0f%%", ((double) delta_active) / (delta_active + delta_idle) * 100);
                         }
-                        printf(" %6lld/%-6lld", active-cpu_stats[cpu].active, idle-cpu_stats[cpu].idle);
-                        cpu_stats[cpu].active = active;
-                        cpu_stats[cpu].idle = idle;
-                        cpu_stats[cpu].iowait = iowait;
-                        cpu_stats[cpu].irq = irq;
-                        sprintf(buf, "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq", cpu);
+                        printf(" %6lld/%-6lld", active-cpu_stats[cpu_id].active, idle-cpu_stats[cpu_id].idle);
+                        cpu_stats[cpu_id].active = active;
+                        cpu_stats[cpu_id].idle = idle;
+                        cpu_stats[cpu_id].iowait = iowait;
+                        cpu_stats[cpu_id].irq = irq;
+                        sprintf(buf, "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq", cpu_nr);
                         if ((f2 = fopen(buf, "r")) != NULL) {
                             unsigned freq;
                             fscanf(f2, "%u", &freq);
                             printf(" %7u", freq);
                             fclose(f2);
                         }
-                        cpu++;
+                        cpu_id++;
                 } else if (strncmp(buf, "procs_running", 13) == 0) {
                         unsigned running = atoi(&buf[13]);
                         printf(" %d", running);
